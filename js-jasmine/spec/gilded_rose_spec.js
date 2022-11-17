beb95cc133081bdc9dd8b556315613aaa7257e4d
ChrisPfeiffer <chris.cp147@gmail.com>
2022-11-17 09:09:39 +0100

    modif

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,4 +1,5 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+import {Shop, Item} from '../src/gilded_rose.js'
+
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
@@ -9,15 +10,18 @@ describe("Gilded Rose", function() {
 
 });
 
-describe('Backstage passes', () => {
-  it("should increase quality +2", function() {
-    const gildedRose = new Shop([ new Item("Backstage passes", 9, 2) ]);
+describe("Aged Brie", () => {
+
+  it("ne devrait jamais avoir sa qualité supérieure à 50", () => {
+    const gildedRose = new Shop([ new Item("Aged Brie", 2, 50) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(4);
+    expect(items[0].name).toEqual("Aged Brie");
+    expect(items[0].quality).toEqual(50);
   });
-})
+});
 
 describe('Backstage passes ', () => {
+  
   it("should increase quality +3", function() {
     const gildedRose = new Shop([ new Item("Backstage passes", 4, 2) ]);
     const items = gildedRose.updateQuality();
@@ -26,6 +30,7 @@ describe('Backstage passes ', () => {
 })
 
 describe('Backstage passes', () => {
+ 
   it("should destroy quality if sellIn is passed", function() {
     const gildedRose = new Shop([ new Item("Backstage passes", 0, 30) ]);
     const items = gildedRose.updateQuality();
@@ -34,6 +39,7 @@ describe('Backstage passes', () => {
   });
 })
 describe('Sulfuras', () => {
+  
   it("should never decrease quality", function() {
     const gildedRose = new Shop([ new Item("Sulfuras", 0, 80) ]);
     const items = gildedRose.updateQuality();
@@ -42,6 +48,7 @@ describe('Sulfuras', () => {
   });
 })
 describe('Aged Brie', () => {
+ 
   it("should increase quality", function() {
     const gildedRose = new Shop([ new Item("Aged Brie", 5, 40) ]);
     const items = gildedRose.updateQuality();
@@ -49,7 +56,9 @@ describe('Aged Brie', () => {
 
   });
 })
+
 describe('Conjured', () => {
+  
   it("should decrease quality two times faster", function() {
     const gildedRose = new Shop([ new Item("Conjured", 0, 40) ]);
     const items = gildedRose.updateQuality();
